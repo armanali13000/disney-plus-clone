77343a9d554af2a0a4fe227b1aed37162620b5bd
armanali13000 <arman.ali.13000@gmail.com>
2023-03-04 02:33:52 +0530

    Movies section Added

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import ImgSlider from './ImgSlider'
 import Viewers from './Viewers'
+import Movies from './Movies'
 
 function Home() {
   return (
@@ -10,6 +11,7 @@ function Home() {
 
       <ImgSlider />
       <Viewers />
+      <Movies />
 
     </Container>
     
